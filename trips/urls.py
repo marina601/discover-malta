ed50be67a930025312def609f0b86296b1e39ebc
marina601 <marina601@live.com>
2021-09-23 12:45:13 +0000

    Modified the url pattern to make it more unique

--- trips/urls.py
+++ trips/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_trips, name='trips'),
     path('<slug:category_slug>/', views.all_trips, name='trips_by_category'),
     path('<slug:category_slug>/<slug:trip_slug>/', views.trip_detail, name='trip_detail'),
+    path('search/', views.search, name='search'),
 ]
