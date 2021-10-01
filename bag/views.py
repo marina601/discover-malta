8962e1e42b152da619bbd4ea68667eda24d3e34f
marina601 <marina601@live.com>
2021-10-01 09:54:53 +0000

    Added functionality to add the trips to the bag

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
 def bag(request):
     return render(request, 'bag/bag.html',)
+
+def add_to_bag(request, trip_id):
+    """
+    Add a quantity of the specific trip
+    to the shopping bag
+    """
+
+    booking_date = request.POST.get('booking_date')
+    redirect_url = request.POST.get('redirect_url')
+    total_adults = int(request.POST.get('total_adults'))
+    num_of_childen = int(request.POST.get('num_of_children'))
+    bag = request.session.get('bag', {})
+
+    if trip_id in list(bag.keys()):
+        bag[trip_id] += total_adults
+    else:
+        bag[trip_id] = total_adults
+    
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
+
+    # Calculate
+    # total_num_tickets
+    # total_price
