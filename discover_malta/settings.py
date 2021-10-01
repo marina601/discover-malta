55709c0ecc1e3f56ca4c86f4d1ed83f3fc8c101d
marina601 <marina601@live.com>
2021-10-01 08:36:29 +0000

    Created a new app, urls and templates

--- discover_malta/settings.py
+++ discover_malta/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'availability',
     'multiselectfield',
     'bookings',
+    'bag',
 ]
 
 MIDDLEWARE = [
--- discover_malta/urls.py
+++ discover_malta/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('trips/', include('trips.urls')),
     path('bookings/', include('bookings.urls')),
+    path('bag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
