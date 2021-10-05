697ee3ec0ad4d6a8f6e629564c3e305fd6bc620a
marina601 <marina601@live.com>
2021-10-05 14:23:33 +0000

    Removed the if else statement which was checking for the number of children tickets

--- bag/views.py
+++ bag/views.py
@@ -22,30 +22,19 @@ def add_to_bag(request, trip_id):
     trip = get_object_or_404(Trip, pk=trip_id)
     bag = request.session.get('bag', {})
 
-    if trip.child_price: 
-        if trip_id in list(bag.keys()):
-            bag[trip_id]['quantity'] += 1
-            bag[trip_id]['booking_date'] = booking_date
-            bag[trip_id]['adult_tickets'] = adult_tickets
-            bag[trip_id]['children_tickets'] = children_tickets
-        else:
-            bag[trip_id] = {
-                'quantity': 1, 'booking_date': booking_date, 'adult_tickets': adult_tickets, 'children_tickets': children_tickets
-            }
-    else:
-        if trip_id in list(bag.keys()):
-            bag[trip_id]['quantity'] += 1
-            bag[trip_id]['booking_date'] = booking_date
-            bag[trip_id]['adult_tickets'] = adult_tickets
-        else:
-            bag[trip_id] = {
-                'quantity': 1, 'booking_date': booking_date, 'adult_tickets': adult_tickets
-            }
-        
-    request.session['bag'] = bag
-    print(request.session["bag"])
-    return redirect(redirect_url)
 
-    # Calculate
-    # total_num_tickets
-    # total_price
+    if trip_id in list(bag.keys()):
+        bag[trip_id]['quantity'] += 1
+        bag[trip_id]['booking_date'] = booking_date
+        bag[trip_id]['adult_tickets'] = adult_tickets
+        bag[trip_id]['children_tickets'] = children_tickets
+        print(children_tickets)
+    else:
+        bag[trip_id] = {
+            'quantity': 1, 'booking_date': booking_date, 'adult_tickets': adult_tickets, 'children_tickets': children_tickets
+        }
+
+    request.session['bag'] = bag
+    # print(children_tickets)
+    # print(request.session["bag"])
+    return redirect(redirect_url)
