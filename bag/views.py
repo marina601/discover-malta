5aa72fab051bdc47dd270c34c5f31dfbaf2d9c67
marina601 <marina601@live.com>
2021-10-05 09:11:07 +0000

    Refuctured the code to handle all the input values from the form

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
+
 from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
+from trips.models import Trip
 
 # Create your views here.
 
@@ -6,27 +9,31 @@ from django.shortcuts import render, redirect
 def bag(request):
     return render(request, 'bag/bag.html',)
 
+
 def add_to_bag(request, trip_id):
     """
     Add a quantity of the specific trip
     to the shopping bag
     """
-
     booking_date = request.POST.get('booking_date')
+    adult_tickets = int(request.POST.get('adult_tickets'))
+    children_tickets = int(request.POST.get('children_tickets'))
     redirect_url = request.POST.get('redirect_url')
-    total_adults = int(request.POST.get('total_adults'))
-    num_of_childen = request.POST.get('num_of_children')
+    trip = get_object_or_404(Trip, pk=trip_id)
     bag = request.session.get('bag', {})
 
     if trip_id in list(bag.keys()):
-        bag[trip_id] += total_adults
-        booking_date = booking_date
-        num_of_childen = num_of_childen
+        bag[trip_id]['quantity'] += 1
+        bag[trip_id]['booking_date'] = booking_date
+        bag[trip_id]['adult_ticket'] = adult_tickets
+        bag[trip_id]['children_tickets'] = children_tickets
     else:
-        bag[trip_id] = total_adults
-    
+        bag[trip_id] = {
+            'quantity': 1, 'booking_date': booking_date, 'adult_tickets': adult_tickets, 'children_tickets': children_tickets
+            }
+        
     request.session['bag'] = bag
-    
+    print(request.session["bag"])
     return redirect(redirect_url)
 
     # Calculate
