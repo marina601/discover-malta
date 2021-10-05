f1cf44f4fc2c190c11c22e4ce9a32c08f77aed73
marina601 <marina601@live.com>
2021-10-05 11:12:48 +0000

    Added if else statment to handle if there are no children ticket selected

--- bag/views.py
+++ bag/views.py
@@ -22,14 +22,24 @@ def add_to_bag(request, trip_id):
     trip = get_object_or_404(Trip, pk=trip_id)
     bag = request.session.get('bag', {})
 
-    if trip_id in list(bag.keys()):
-        bag[trip_id]['quantity'] += 1
-        bag[trip_id]['booking_date'] = booking_date
-        bag[trip_id]['adult_ticket'] = adult_tickets
-        bag[trip_id]['children_tickets'] = children_tickets
+    if trip.child_price: 
+        if trip_id in list(bag.keys()):
+            bag[trip_id]['quantity'] += 1
+            bag[trip_id]['booking_date'] = booking_date
+            bag[trip_id]['adult_tickets'] = adult_tickets
+            bag[trip_id]['children_tickets'] = children_tickets
+        else:
+            bag[trip_id] = {
+                'quantity': 1, 'booking_date': booking_date, 'adult_tickets': adult_tickets, 'children_tickets': children_tickets
+            }
     else:
-        bag[trip_id] = {
-            'quantity': 1, 'booking_date': booking_date, 'adult_tickets': adult_tickets, 'children_tickets': children_tickets
+        if trip_id in list(bag.keys()):
+            bag[trip_id]['quantity'] += 1
+            bag[trip_id]['booking_date'] = booking_date
+            bag[trip_id]['adult_tickets'] = adult_tickets
+        else:
+            bag[trip_id] = {
+                'quantity': 1, 'booking_date': booking_date, 'adult_tickets': adult_tickets
             }
         
     request.session['bag'] = bag
