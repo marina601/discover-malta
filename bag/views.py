05f754e78d76896a58706d45b06bef349225ca49
marina601 <marina601@live.com>
2021-10-08 09:00:50 +0000

    Added a conditional statement to update the bag if adult tickets exist

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.shortcuts import get_object_or_404
 from trips.models import Trip
@@ -17,25 +16,34 @@ def add_to_bag(request, trip_id):
     """
     booking_date = request.POST.get('booking_date')
     adult_tickets = int(request.POST.get('adult_tickets'))
-    children_tickets = int(request.POST.get('children_tickets'))
+    children_tickets = int(0)
     redirect_url = request.POST.get('redirect_url')
     # trip = get_object_or_404(Trip, pk=trip_id)
+
+    # Check it the children tickets are in the request
+    if 'children_tickets' in request.POST:
+        children_tickets = int(request.POST['children_tickets'])
     bag = request.session.get('bag', {})
 
     if trip_id in list(bag.keys()):
+        """
+        Add items to the bag
+        """
         bag[trip_id]['quantity'] += 1
         bag[trip_id]['booking_date'] = booking_date
         bag[trip_id]['adult_tickets'] = adult_tickets
         bag[trip_id]['children_tickets'] = children_tickets
-        print(children_tickets)
     else:
         bag[trip_id] = {
-            'quantity': 1, 'booking_date': booking_date, 'adult_tickets': adult_tickets, 'children_tickets': children_tickets
+            'quantity': 1, 'booking_date': booking_date,
+            'adult_tickets': adult_tickets,
+            'children_tickets': children_tickets,
         }
 
     request.session['bag'] = bag
-    # print(children_tickets)
-    # print(request.session["bag"])
+    # del request.session['bag']
+    print(f'bag is: {bag}')
+
     return redirect(redirect_url)
 
 
@@ -44,22 +52,23 @@ def update_bag(request, trip_id):
     Add a quantity of the specific trip
     to the shopping bag
     """
-
     adult_tickets = int(request.POST.get('adult_tickets'))
-    children_tickets = int(request.POST.get('children_tickets'))
+    children_tickets = int(0)
+   
+    # Check it the children tickets are in the request
+    # check if adult_tickets exist
+    # else delete the trip
+
+    if 'children_tickets' in request.POST:
+        children_tickets = int(request.POST['children_tickets'])
     bag = request.session.get('bag', {})
 
-    if adult_tickets > 0:
-        bag[trip_id]['adult_tickets'] = int(adult_tickets)
-        bag[trip_id]['children_tickets'] = int(children_tickets)
+    if adult_tickets:
+        if adult_tickets > 0:
+            bag[trip_id]['adult_tickets'] = int(adult_tickets)
+            bag[trip_id]['children_tickets'] = int(children_tickets)
     else:
-        del bag[trip_id]['adult_tickets']
-        del bag[trip_id]['children_tickets']
-    
-    if children_tickets > 0:
-        bag[trip_id]['children_tickets'] = int(children_tickets)
-    else:
-        del bag[trip_id]['children_tickets']
+        del bag[trip_id]
 
     request.session['bag'] = bag
     return redirect(reverse('bag'))
