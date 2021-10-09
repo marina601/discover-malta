9de8f1fa4d45a0063769ba5bec2fe827829a018b
marina601 <marina601@live.com>
2021-10-09 16:22:06 +0000

    Added a check on line 31 to update the quantity if booking date and trip id already in the bag

--- bag/views.py
+++ bag/views.py
@@ -28,22 +28,26 @@ def add_to_bag(request, trip_id):
     bag = request.session.get('bag', {})
 
     if trip_id in list(bag.keys()):
-        """
-        Add items to the bag
-        """
-        bag[trip_id]['quantity'] += 1
-        bag[trip_id]['booking_date'] = booking_date
-        bag[trip_id]['adult_tickets'] = adult_tickets
-        bag[trip_id]['children_tickets'] = children_tickets
-        messages.success(request,
-                                (f'Updated your suitcase with {trip.name}'))
+        if booking_date == bag[trip_id]['booking_date']:
+            """
+            Add items to the bag
+            """
+            bag[trip_id]['quantity'] += 1
+            bag[trip_id]['booking_date'] = booking_date
+            bag[trip_id]['adult_tickets'] += adult_tickets
+            bag[trip_id]['children_tickets'] += children_tickets
+            messages.success(request,
+                             (f'we Have updated your suitcase for {trip.name}'
+                              f' and {booking_date} date with {adult_tickets}'
+                              f' adult tickets'
+                              f' and {children_tickets} children tickets'))
     else:
         bag[trip_id] = {
             'quantity': 1, 'booking_date': booking_date,
             'adult_tickets': adult_tickets,
             'children_tickets': children_tickets,
         }
-        messages.success(request, f'Added {trip.name} to your suitcase')
+        messages.success(request, (f'You added {trip.name}, on {booking_date} date to your suitcase'))
 
     request.session['bag'] = bag
     # del request.session['bag']
