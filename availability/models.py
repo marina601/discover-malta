525dbbf349ea65a5cf4507a7994e6d2375f1dffc
marina601 <marina601@live.com>
2021-10-12 13:21:40 +0000

    Removed unused code

--- availability/models.py
+++ availability/models.py
@@ -8,7 +8,6 @@ from trips.models import Trip
 
 
 class Ticket(models.Model):
-
     # comes from the trips
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
     booking_date = models.DateField(default=timezone.now)
@@ -18,6 +17,6 @@ class Ticket(models.Model):
     total_price = models.PositiveIntegerField(default=0)
 
     def __str__(self):
-        return '{} {} {} {} {}'.format(self.booking_date, self.total_num_tickets, 
-                                       self.total_price,
-                                       self.trip.adult_price, self.trip.child_price)
+        return '{} {} {}'.format(self.booking_date,
+                                 self.total_num_tickets, 
+                                 self.total_price)
