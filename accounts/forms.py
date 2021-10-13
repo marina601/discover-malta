f730e793ce4de099dde7d57b61cb3bf1cb587d50
marina601 <marina601@live.com>
2021-10-13 11:53:28 +0000

    Added a conditional check to ensure the 2 passwords fields match

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,12 +12,22 @@ class RegistraionForm(forms.ModelForm):
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Repeat Password'
     }))
+    phone_number = forms.IntegerField(required=True)
 
     class Meta:
         """Create a model form"""
         model = Account
-        fields = ['first_name', 'last_name', 'phone_number',
+        fields = ['first_name', 'last_name',
                   'email', 'password']
+
+    def clean(self):
+        """Check the passwords match"""
+        cleaned_data = super(RegistraionForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError("Your passwords do not match")
 
     def __init__(self, *args, **kwargs):
         """Add bootstrap classes to all the input fields"""
