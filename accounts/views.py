63e05fd954a0e29e27c6f40a63f38093258626dd
marina601 <marina601@live.com>
2021-10-13 10:00:04 +0000

    Added a functionality to create a new user

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,36 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 from .forms import RegistraionForm
+from .models import Account
 
 # Create your views here.
 
 
+@csrf_exempt
 def register(request):
     """
     Register function
     """
-    form = RegistraionForm
+    if request.method == 'POST':
+        form = RegistraionForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user = Account.objects.create_user(first_name=first_name,
+                                               last_name=last_name,
+                                               email=email,
+                                               username=username,
+                                               password=password,)
+            user.phone_number = phone_number
+            user.save()
+    else:
+        form = RegistraionForm()
+
     context = {
         'form': form,
     }
