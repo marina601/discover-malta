c9871653cbaafab59c0293cd433f4d8e519f10ae
marina601 <marina601@live.com>
2021-10-18 09:24:09 +0000

    Added stripe variables to the view and created payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -3,23 +3,45 @@ from django.contrib import messages
 
 from .models import Order, OrderTicketItem
 from .forms import OrderForm
+from bag.contexts import bag_content
+from django.conf import settings
 # Create your views here.
 
+import stripe
 
 def checkout(request):
     """Checkout"""
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    # getting the bag from the session
     bag = request.session.get('bag', {})
     if not bag:
         # If the bag is empty
         messages.error(request, "You do not have any items in your bag at the moment")
         return redirect(reverse('trips'))
     
+    current_bag = bag_content(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Set up your public key')
+    
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51JP0yCCBQ5fEMNSJGipjZTFTGX5b31SEdf8qACJwjVRBxVW46gczsPOJdALrVhg28WrMEVhy0nBco7rHNICB6LtT00P2ZrUJiG',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
