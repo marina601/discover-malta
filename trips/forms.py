855043cf60e288ca7e0b9f494e3009ea71e4ff21
marina601 <marina601@live.com>
2021-10-23 16:21:36 +0000

    Created a review and rating form

--- trips/forms.py
+++ trips/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Trip, Category
+from .models import Trip, Category, ReviewRating
 
 
 class TripForm(forms.ModelForm):
@@ -17,3 +17,9 @@ class TripForm(forms.ModelForm):
         for field_name, field in self.fields.items():
             """ Add classes to the form fields"""
             field.widget.attrs['class'] = 'form-control'
+
+
+class ReviwForm(forms.ModelForm):
+    class Meta:
+        models = ReviewRating
+        fields = ['subject', 'review', 'rating']
