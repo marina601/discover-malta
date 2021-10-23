ffb3b6c5a78963f774b8ac85cf6176ef9597f64e
marina601 <marina601@live.com>
2021-10-23 14:49:51 +0000

    Created a model for trip review and rating

--- trips/admin.py
+++ trips/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Trip
+from .models import Category, Trip, ReviewRating
 
 # Register your models here.
 
@@ -20,3 +20,4 @@ class TripAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Trip, TripAdmin)
+admin.site.register(ReviewRating)
--- trips/models.py
+++ trips/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 
+from accounts.models import Account
+
 # Create your models here.
 
 
@@ -67,3 +69,19 @@ class Trip(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ReviewRating(models.Model):
+    """Rating and Review Model"""
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=50, blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    rating = models.FloatField()
+    ip = models.CharField(max_length=20, blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
