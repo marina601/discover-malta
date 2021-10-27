98f8aba53ee86f976095929bce3fb390f6ea29c7
marina601 <marina601@live.com>
2021-10-27 10:03:27 +0000

    Removed unused apps

--- bookings/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from django import forms
-from availability.models import Ticket
-
-
-class AvailabilityForm(ModelForm):
-    class Meta:
-        model = Ticket
-        fields = ['trip', 'booking_date', 'total_adults', 'num_of_childen']
--- bookings/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<int:id>/view_availability/', views.view_availability, name='view_availability'), 
-]
