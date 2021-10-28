394879b5d83ee55b3b5751058c7313100d8f341a
marina601 <marina601@live.com>
2021-10-28 10:29:24 +0000

    Removed white space and fix the logn lines errors

--- trips/urls.py
+++ trips/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.all_trips, name='trips'),
-    path('category/<slug:category_slug>/', views.all_trips, name='trips_by_category'),
-    path('category/<slug:category_slug>/<slug:trip_slug>/', views.trip_detail, name='trip_detail'),
+    path('category/<slug:category_slug>/', views.all_trips,
+         name='trips_by_category'),
+    path('category/<slug:category_slug>/<slug:trip_slug>/', views.trip_detail,
+         name='trip_detail'),
     path('search/', views.search, name='search'),
     path('add_trip/', views.add_trip, name='add_trip'),
-    path('submit_review/<int:trip_id>/<int:user_id>/', views.submit_review, name='submit_review'),
+    path('submit_review/<int:trip_id>/<int:user_id>/', views.submit_review,
+         name='submit_review'),
 ]
