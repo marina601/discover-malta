566fe9310e24f9c822fc474406b9fd1e7bf00d9c
marina601 <marina601@live.com>
2021-11-03 18:54:35 +0000

    Added missing semicolon on line 116

--- checkout/static/checkout/js/stripe.js
+++ checkout/static/checkout/js/stripe.js
@@ -1,3 +1,6 @@
+//jshint esversion: 6
+/*globals $:false */
+
 /*
     Core logic/payment flow for this comes from here:
     https://stripe.com/docs/js
@@ -110,5 +113,5 @@ form.addEventListener('submit', function(ev) {
     }).fail(function() {
         // just reload the page, the error will be displayed to the user in the messages
         location.reload();
-    })
+    });
 });
