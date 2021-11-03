73aaa289b2adbf165f7e9f2e920be0072b7f768d
marina601 <marina601@live.com>
2021-11-03 20:20:30 +0000

    Re-factored the code to validate the code with PEP8

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -29,12 +29,12 @@ def webhook(request):
         )
     except ValueError as e:
         # Invalid payload
-        return HttpResponse(status=400)
+        return HttpResponse(status=400, e=e)
     except stripe.error.SignatureVerificationError as e:
         # Invalid Signature
-        return HttpResponse(status=400)
+        return HttpResponse(status=400, e=e)
     except Exception as e:
-        return HttpResponse(content=e, status=400, e=e)
+        return HttpResponse(content=e, status=400)
 
     # Set up a webhook handler
     handler = StripeWH_Handler(request)
@@ -42,7 +42,8 @@ def webhook(request):
     # Map webhook events to relevant handler functions
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+        'payment_intent.payment_failed':
+        handler.handle_payment_intent_payment_failed,
     }
 
     # Get the webhook type from Stripe
