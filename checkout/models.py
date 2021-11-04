cdedb1cbd87f3ca95a7a041f362cd66d2d6a59f9
marina601 <marina601@live.com>
2021-11-04 13:38:16 +0000

    Added null=True to county, street_address2 and postcode fields

--- checkout/models.py
+++ checkout/models.py
@@ -25,13 +25,13 @@ class Order(models.Model):
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     country = CountryField(blank_label='Country *', null=False, blank=False)
-    postcode = models.CharField(max_length=20, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
     # not required field
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
-    street_address2 = models.CharField(max_length=80, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
     # not required field
-    county = models.CharField(max_length=80, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
     # auto_now_add field will automatically set order date and time
     date = models.DateTimeField(auto_now_add=True)
     # these fields will be calculated using the model method
