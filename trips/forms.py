92549ff0270a29d69d8d058c3d6bae8d55c6e0f7
marina601 <marina601@live.com>
2021-11-04 08:38:18 +0000

    Added custom class to ReviewForm fields

--- trips/forms.py
+++ trips/forms.py
@@ -26,3 +26,10 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = ReviewRating
         fields = ['subject', 'review', 'rating']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+        for field_name, field in self.fields.items():
+            """ Add classes to the form fields"""
+            field.widget.attrs['class'] = 'form-control'
