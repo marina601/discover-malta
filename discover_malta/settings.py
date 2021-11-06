c2c90d4c75a5ab64139179c9f4211f486ec757af
marina601 <marina601@live.com>
2021-11-06 22:53:16 +0000

    Changed debut settings

--- discover_malta/settings.py
+++ discover_malta/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['discover-malta.herokuapp.com', 'localhost']
 
