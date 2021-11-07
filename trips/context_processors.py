dfafa9b2d55cb21db4185938dac6a91400016bbe
marina601 <marina601@live.com>
2021-11-07 17:52:59 +0000

    Added doc-string to the function

--- trips/context_processors.py
+++ trips/context_processors.py
@@ -1,6 +1,8 @@
+# pylint: disable=no-member
 from .models import Category
 
 
 def menu_links(request):
+    """Get links to all categories"""
     links = Category.objects.all()
     return dict(links=links)
