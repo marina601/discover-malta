a965128d293997fb7f9be45f35aeae41eff60321
marina601 <marina601@live.com>
2021-11-07 20:30:46 +0000

    Added min/max value for the review, refactored the code to comply with PEP8

--- trips/models.py
+++ trips/models.py
@@ -1,30 +1,34 @@
+# pylint: disable=no-member
 from django.db import models
 from django.db.models import Avg, Count
 from django.urls import reverse
 from django.utils import timezone
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 from accounts.models import Account
 
-# Create your models here.
-
 
 class Category (models.Model):
+    """Category Model"""
     category_name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     cat_image = models.ImageField(upload_to='media/categories', blank=True)
 
     class Meta:
+        """Modify Plural Name"""
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
     def get_url(self):
+        """url for categories"""
         return reverse('trips_by_category', args=[self.slug])
 
     def __str__(self):
-        return self.category_name
+        return str(self.category_name)
 
 
 class Trip(models.Model):
+    """Trip Model"""
 
     START_TIME = [
         ('06:00', '06:00'),
@@ -57,8 +61,10 @@ class Trip(models.Model):
     add_to_favourites = models.ManyToManyField(Account,
                                                related_name='favourites',
                                                default=None, blank=True)
-    adult_price = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)
-    child_price = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)
+    adult_price = models.DecimalField(max_digits=6, decimal_places=2,
+                                      default=0.0)
+    child_price = models.DecimalField(max_digits=6, decimal_places=2,
+                                      default=0.0)
     images = models.ImageField(upload_to='media/trips')
     num_tickets = models.PositiveIntegerField(default=0)
     from_date = models.DateField(default=timezone.now)
@@ -68,22 +74,27 @@ class Trip(models.Model):
     modified_date = models.DateTimeField(auto_now=True)
 
     def get_url(self):
+        """url for trip detail"""
         return reverse('trip_detail', args=[self.category.slug, self.slug])
 
     def __str__(self):
-        return self.name
-    
+        return str(self.name)
+
     def averageRating(self):
-        # Calculate average trip review
-        reviews = ReviewRating.objects.filter(trip=self, status=True).aggregate(average=Avg('rating'))
+        """Calculate average trip review"""
+        reviews = ReviewRating.objects.filter(trip=self,
+                                              status=True).aggregate(
+                                                  average=Avg('rating'))
         avg = 0
         if reviews['average'] is not None:
             avg = float(reviews['average'])
         return avg
-    
+
     def totalReviews(self):
-        # Calculate total reviews
-        reviews = ReviewRating.objects.filter(trip=self, status=True).aggregate(count=Count('id'))
+        """Calculate total reviews"""
+        reviews = ReviewRating.objects.filter(trip=self,
+                                              status=True).aggregate(
+                                                  count=Count('id'))
         count = 0
         if reviews['count'] is not None:
             count = int(reviews['count'])
@@ -95,7 +106,9 @@ class ReviewRating(models.Model):
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
     subject = models.CharField(max_length=50, blank=True)
-    review = models.TextField(max_length=500, blank=True)
+    review = models.TextField(max_length=500, blank=True,
+                              validators=[MinValueValidator(0.0),
+                                          MaxValueValidator(5.0)])
     rating = models.FloatField(blank=True)
     ip = models.CharField(max_length=20, blank=True)
     status = models.BooleanField(default=True)
@@ -103,4 +116,4 @@ class ReviewRating(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.subject
+        return str(self.subject)
