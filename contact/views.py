bba9f83b4b0d54e878a86f2dae912d5a912a21b7
marina601 <marina601@live.com>
2021-11-09 23:09:44 +0000

    Added functionality to send automated emails to the user and admin when the form is being submitted

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from django.core.mail import EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+
+from django.conf import settings
 
 # Create your views here.
 
 
 def contact(request):
-    return render(request, 'contact/contact.html')
+    """Render contact page and send confirmation email"""
+    full_name = None
+    email = None
+    subject = None
+    message = None
+
+    if request.method == "POST":
+        enquire = request.POST['message']
+        email = request.POST['email']
+        full_name = request.POST['full_name']
+        subject = request.POST['subject']
+
+        # Send an email to site owner
+        current_site = get_current_site(request)
+        mail_subject = subject
+        message = render_to_string(
+            'emails/enquire.html', {
+                'enquire': enquire,
+                'email': email,
+                'full_name': full_name,
+                'domain': current_site,
+            })
+        to_email = settings.EMAIL_HOST_USER, settings.DEFAULT_FROM_EMAIL
+        send_email = EmailMessage(mail_subject, message, to=[to_email])
+        send_email.send()
+
+        # Send a confirmation email to the user
+        current_site = get_current_site(request)
+        mail_subject = subject
+        message = render_to_string(
+            'emails/confirmation_email.html', {
+                'domain': current_site,
+                'enquire': enquire,
+                'email': email,
+                'full_name': full_name,
+            })
+        to_email = email
+        send_email = EmailMessage(mail_subject, message, to=[to_email])
+        send_email.send()
+        messages.success(request, f"Thank you {full_name} for your message,"
+                         "Somebody from our team will get in touch shortly")
+        return redirect('home')
+
+    context = {
+        'full_name': full_name,
+        'email': email,
+        'subject': subject,
+        'message': message,
+    }
+
+    return render(request, 'contact/contact.html', context)
