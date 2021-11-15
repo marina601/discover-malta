7eba0f0c812672c7871883c7905eedbf2acdce05
marina601 <marina601@live.com>
2021-11-15 22:31:41 +0000

    Added a default from email to settings

--- discover_malta/settings.py
+++ discover_malta/settings.py
@@ -198,6 +198,7 @@ else:
     EMAIL_PORT = 587
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
     EMAIL_USE_TLS = True
 
 # STRIPE
