6f04b490672e394ddba442a4094814f93ec853b3
marina601 <marina601@live.com>
2021-11-15 12:14:49 +0000

    Formated the document and added $:false for validation

--- static/js/datepicker.js
+++ static/js/datepicker.js
@@ -1,9 +1,12 @@
+ //jshint esversion: 6
+ /*globals $:false */
+
+ 
  // Code taken from https://api.jqueryui.com/datepicker/
- $( function() {
-    $( ".calendar" ).datepicker({
-        dateFormat : 'yy-mm-dd',
-        minDate : new Date(),
-        firstDay: 1,
-    });
-   
-  } );
+ $(function () {
+   $(".calendar").datepicker({
+     dateFormat: 'yy-mm-dd',
+     minDate: new Date(),
+     firstDay: 1,
+   });
+ });
