21df5f4769ee23f0b175a32847f68de8781ee201
marina601 <marina601@live.com>
2021-11-15 12:21:25 +0000

    Removed commented out code

--- trips/static/trips/js/modal.js
+++ trips/static/trips/js/modal.js
@@ -1,13 +1,10 @@
 //jshint esversion: 6
-// Modal from bootstrap adding focus to an input
+
+
+// Modal from bootstrap adding focus to an input element
 var myModal = document.getElementById('exampleModal');
 var myInput = document.getElementById('total_adults');
 
 myModal.addEventListener('shown.bs.modal', function () {
     myInput.focus();
 });
-
-// //Delete Modal
-// $('#myModal').on('shown.bs.modal', function () {
-//     $('#myInput').trigger('focus');
-// });
