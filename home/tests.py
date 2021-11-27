d63a14ea56ce99326bdbc67990b75583c693bde0
marina601 <marina601@live.com>
2021-11-27 12:54:40 +0000

    Created a unit test for home view

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestHomeView(TestCase):
+    """Unit Test for home view"""
+
+    def test_home_view(self):
+        """Test HTTP response"""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
