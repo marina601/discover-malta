5ce56e0fa6cbb31396b4d8fa93c08a36bfa27508
marina601 <marina601@live.com>
2021-11-27 12:51:52 +0000

    Added unit testing for http response

--- contact/tests.py
+++ contact/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class TestHomeView(TestCase):
+
+    def test_contact_view(self):
+        """Test HTTP of contact view response"""
+        response = self.client.get('/contact/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact/contact.html')
+    
+    def test_about_view(self):
+        """Test HTTP of about view response"""
+        response = self.client.get('/contact/about/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about/about.html')
