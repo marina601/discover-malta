1a27d18677880d75e2707625735ee3218e0e4079
marina601 <marina601@live.com>
2021-11-28 13:55:30 +0000

    Formatted a document

--- trips/static/trips/js/modal.js
+++ trips/static/trips/js/modal.js
@@ -2,14 +2,13 @@
 
 
 /* Modal from bootstrap adding focus to an input element
-* Check if the element exists
-*/
+ * Check if the element exists
+ */
 var myModal = document.getElementById('exampleModal');
 var myInput = document.getElementById('total_adults');
 
-if(myModal) {
+if (myModal) {
     myModal.addEventListener('shown.bs.modal', function () {
-       myInput.focus();
+        myInput.focus();
     });
 }
-
